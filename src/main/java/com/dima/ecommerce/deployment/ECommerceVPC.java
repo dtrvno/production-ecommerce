d25e84e494079148b4f89e3bf02ef339c3c9ef8a
dtrvno <dimatru@gmail.com>
2021-11-09 00:02:28 -0800

    continue init

---
+++ src/main/java/com/dima/ecommerce/deployment/ECommerceVPC.java
@@ -0,0 +1,8 @@
+package com.dima.ecommerce.deployment;
+
+//parameters name and cidr block
+public class ECommerceVPC {
+
+ //   aws ec2 create-vpc --cidr-block 10.0.0.0/16
+ //aws ec2 create-tags --resources vpc-016f7f2119b9e2764 --tags Key=Name,Value=Production
+}
---
+++ src/main/java/com/dima/ecommerce/status/ECommerceStatus.java
@@ -0,0 +1,8 @@
+package com.dima.ecommerce.status;
+
+public class ECommerceStatus {
+    // json file provides status of deployment
+    /*
+
+     */
+}
